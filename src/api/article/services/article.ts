0ab0b69c3078406ef43f2c7f58d55e1e1790169e
Pradit Yamsook <praditcry@gmail.com>
2025-02-24 21:10:56 +0700

    [FIX]: fix query

--- src/api/article/services/article.ts
+++ src/api/article/services/article.ts
@@ -4,4 +4,26 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreService('api::article.article');
+export default factories.createCoreService('api::article.article', ({ strapi }) => ({
+    async findArticleWithProductPriceGreaterThan(minPrice: number = 0): Promise<any> {
+        const articles = await strapi.documents('api::article.article').findMany({
+            populate: {
+                products: {
+                    filters: {
+                        price: {
+                            $gt: minPrice
+                        }
+                    }
+                }
+            }
+        })
+
+        strapi.log.log('articles', articles);
+
+        // const filteredArticles = articles.filter((article: any) => {
+        //     return article.relatedProducts.some((product: any) => product.price > minPrice);
+        // }
+
+        return articles;
+    },
+}));
